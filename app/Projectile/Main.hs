e631286072018b2cb9df601592e37356e2d37ba3
Karl Frederick Roldan <karlfroldan@gmail.com>
2022-07-02 16:21:14 +0800

    rename tick

--- app/Projectile/Main.hs
+++ app/Projectile/Main.hs
@@ -12,15 +12,15 @@ main :: IO ()
 main = do 
     let p = Projectile (point 0 1 0) (normalize (vector 1 1 0)) :: Projectile Double
         e = Environment (vector 0 (-0.1) 0) (vector (-0.01) 0 0) :: Environment Double 
-    keepTicking e p
+    tickIO e p
 
-keepTicking :: (Show a, Num a, Ord a) => Environment a -> Projectile a -> IO ()
-keepTicking e p = do 
+tickIO :: (Show a, Num a, Ord a) => Environment a -> Projectile a -> IO ()
+tickIO e p = do 
     let Tuple4 _ yPos  _ _ = getPosition p
     unless (yPos <= 0) $ do 
         putStrLn $ show $ getPosition p
         let newProj = tick e p 
-        keepTicking e newProj
+        tickIO e newProj
 
 tick :: Num a => Environment a -> Projectile a -> Projectile a 
 tick (Environment gravity wind) (Projectile position velocity) = 
