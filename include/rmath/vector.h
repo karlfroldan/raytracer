3a151d9a17bf8310647f8b0aafd48883ae90ddc3
karlfroldan <karlfroldan@gmail.com>
2022-07-16 19:21:14 +0800

    update

--- include/rmath/vector.h
+++ include/rmath/vector.h
@@ -1,6 +1,40 @@
 #ifndef RMATH_VECTOR_H
 #define RMATH_VECTOR_H
 
-#define vector(...)
+/* An epsilon for approximated equality between two numbers. */
+const double EPSILON = 0.00001;
+
+/* Compare whether two doubles are approx. */
+int approx_d(double, double);
+
+/* 
+ * A vector is a fixed-size array of numbers closed under the following operations:
+ * 1. Vector-to-vector addition
+ * 2. Vector-to-scalar/scalar-to-vector multiplication.
+ */
+struct vec
+{
+    double *arr; /* Our implementation of a vector can only contain doubles. */
+    int    size; /* How many elements are there in our vector. */
+};
+
+/*********************
+ * VECTOR OPERATIONS *
+ *********************/
+
+/* 
+ * Compares whether two vectors are approximated from each other. 
+ * If their sizes are not equal, return 0.
+ */
+int approx_v(struct vec*, struct vec*);
+
+/* Add two vectors. */
+struct vec vec_addition(struct vec*, struct vec*);
+
+/* Multiply a scalar and a vector. */
+struct vec scalar_vec_mult(double, struct vec*);
+
+int is_point(struct vec*);
+int is_vector(struct vec*);
 
 #endif /* RMATH_VECTOR_H */
