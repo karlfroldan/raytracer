24fc3d8ee047e3b2e3d11e327059a5fa8d331716
karlfroldan <karlfroldan@gmail.com>
2022-07-16 17:11:23 +0800

    move to c

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for raytracer
-
-## Unreleased changes
--- test/Spec.hs
+++
@@ -1,9 +0,0 @@
-import Test.Hspec
-
-import Test.Raytracer.Transformation
-import Test.Raytracer.Rays
-
-main :: IO ()
-main = hspec $ do
-    transTests
-    raysTests
